50e06c163e8b63e6808ed1cc6ec6353d093c4f42
Ilya Kaplan <ik@alphaguilty.io>
2024-10-23 23:55:20 -0400

    fix(cors): configure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { AppEnv } from "./types";
 import { Hono } from "hono";
 import { auth } from "./modules/auth/auth.controller";
+import { cors } from "hono/cors";
 import { user } from "./modules/user/user.controller";
 
 const app = new Hono<AppEnv>();
+
+app.use(cors());
 
 app.get("/health", (c) => {
   return c.json({ status: "ok" });
